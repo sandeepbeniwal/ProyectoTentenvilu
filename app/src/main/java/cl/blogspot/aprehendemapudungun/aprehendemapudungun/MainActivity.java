813e627622f13d8e5d1dae3a7881a587cfbcb023
Germán Riveros <germanriveros@MacBook-Pro-de-German.local>
2016-07-07 16:10:44 -0400

    Licencia

--- app/src/main/java/cl/blogspot/aprehendemapudungun/aprehendemapudungun/MainActivity.java
+++ app/src/main/java/cl/blogspot/aprehendemapudungun/aprehendemapudungun/MainActivity.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * Esta obra está sujeta a la licencia Reconocimiento-NoComercial-CompartirIgual
+ * 4.0 Internacional de Creative Commons. Para ver una copia de esta licencia,
+ * visite http://creativecommons.org/licenses/by-nc-sa/4.0/.
+ *
+ */
+
 package cl.blogspot.aprehendemapudungun.aprehendemapudungun;
 
 import android.support.v7.app.AppCompatActivity;
